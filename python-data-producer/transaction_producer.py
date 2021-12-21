2305e96d77666d899609a4e564cc3d012f4d2c97
Mark Yates <mark.yates@smoothstack.com>
2021-12-21 09:27:22 -0700

    cloud : cleanup and env setup

--- python-data-producer/transaction_producer.py
+++ python-data-producer/transaction_producer.py
@@ -8,17 +8,13 @@ def populate_transaction(auth, acc_nums):
     transaction_entries = len(acc_nums)
     for i in range(transaction_entries):
         register_info = {
-            "amount" : fake.numerify('#####'), # int
-            "date" : fake.numerify('201#-0%-1#'), # datetime(6) nullable
-            "initialBalance" : fake.numerify('####'), # int
-            "method" : fake.random_element(elements=('ACH','ATM','CREDIT_CARD','DEBIT_CARD','APP')), # varchar(255)
+            "amount" : fake.numerify('#####'),
+            "date" : fake.numerify('201#-0%-1#'),
+            "initialBalance" : fake.numerify('####'),
+            "method" : fake.random_element(elements=('ACH','ATM','CREDIT_CARD','DEBIT_CARD','APP')), 
             "merchantCode" : '1111',
-            "state" : fake.random_element(elements=('CREATED','PROCESSING')), # varchar(255)
-            "status" : fake.random_element(elements=('APPROVED','DENIED','PENDING')), # varchar(255)
-            "type" : fake.random_element(elements=('WITHDRAWAL','TRANSFER_OUT','TRANSFER_IN','DEPOSIT')), # varchar(255)
+            "type" : fake.random_element(elements=('WITHDRAWAL','TRANSFER_OUT','TRANSFER_IN','DEPOSIT')),
             "accountNumber" : acc_nums[i]
-            # account_id is restricted foreign key from table 'account'
-            # merchant_code is restricted foreign key from table 'merchant'
         }
         reg_trans = requests.post(register_url, json=register_info, headers=auth)
         # print(reg_trans.text)
